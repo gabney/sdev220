b2f05e2b5112bb52255cefc9c40d5ed1b8985547
gabney <gabney@ivytech.edu>
2025-09-17 19:36:22 -0400

    finish tutorial, add validation
    finalized the tutorial code,
    added validation of code working

--- Module03/M03 Tutorial/M03 Tutorial - Functional vs OOP Programming.py
+++ Module03/M03 Tutorial/M03 Tutorial - Functional vs OOP Programming.py
@@ -11,7 +11,12 @@ the data in a list to be sorted numerically.  This is accomplished by \
 adding up all the incidences of 0, 1, or 2 in the original list, \
 then populating a new list starting from lowest number.
  
-The second exercise, "Binary Search", has us 
+The second exercise, "Binary Search", has us find the index value for \
+a number in a sorted list.  This is accomplised by finding the midpoint \
+of the inputted list's length, then determining if the list value is equal to, \
+less than, or greater than the search value.  If not equal, either the \
+half of the list above or below is then searched for a midpoint and compared \
+iteratively until the value matches or the list is fully searched.
 
 """
 
@@ -56,8 +61,40 @@ unsorted_test = [0,2,1,0,0,1,2,0,2,0,1,2,1,0,2,1,0,1,2,0,1,2,0,1,0,2,0,1,2]
 sorted_test = sort_nums(unsorted_test)
 
 #prints sorted list for user
-print(sorted_test)
+print(f"Your sorted list is {sorted_test}.")
 
 
 
 # Exercise "Binary Search"
+
+def binary_search(unsearched_list, search_for): # creates search function with list to search and value to search for
+    """
+    This function accepts an organized list and a value, and searches \
+    for the value in that list using binary search technique.
+    """
+
+    low = 0 # lowest index value of list to search, initially 0
+    high = len(unsearched_list) - 1 # highest index value of list to search, initially set to the list length - 1, the last value in the list
+    
+    while low <= high: # loop to set mid value, check mid value against the search value, and adjust list search 
+        mid = low + (high - low) // 2 # finds the integer value of the midpoint of list index
+        
+        if unsearched_list[mid] == search_for: #checks to see if the mid value is the search value
+            return mid
+
+        elif unsearched_list[mid] < search_for: # if midpoint is less than search value, moves the lower part of the search to be mid + 1
+            low = mid + 1
+        
+        else: # if midpoint is greater than search value, moves the upper part of the search to be mid - 1
+            high = mid - 1
+    
+    return -1 # returns -1 value if the search value is not found in the list 
+
+#testing the bindary search function
+test_list = [-50, -10, 0, 1, 4, 5, 7, 11, 22, 42, 442, 777, 4042]
+test_value = 777
+
+# stores the result of the bindary search for the test value within the test list
+searh_result = binary_search(test_list, test_value)
+
+print(f"The index value for {test_value} is {searh_result}.")
